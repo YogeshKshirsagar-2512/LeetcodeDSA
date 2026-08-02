1fdb35ea4d4eee3f5838aafe510458503efa7021
Yogesh Kshirsagar <yogeshkshirsagar2304@gmail.com>
2026-08-03 01:25:28 +0530

    Time: 0 ms (100%), Space: 21.9 MB (12.46%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,12 +2,24 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
 
-        int n = nums.size();
-        long sum = (n * (n+1))/2;
-        int vector_sum = 0;
-        for(int ele : nums){
-            vector_sum += ele;
+        // int n = nums.size();
+        // long sum = (n * (n+1))/2;
+        // int vector_sum = 0;
+        // for(int ele : nums){
+        //     vector_sum += ele;
+        // }
+        // return sum - vector_sum;
+
+        vector<bool> flag(nums.size()+1, false);
+        for(int i = 0 ; i < nums.size(); i++){
+            flag[nums[i]] = true;
         }
-        return sum - vector_sum;
+        for(int i =  0 ; i < flag.size(); i++){
+            if(flag[i] == false){
+                return i;
+            }
+        }
+        return nums.size();
+
     }
 };
