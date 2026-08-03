c66c8271dcba69eff51c118ff634d2f621a41738
Yogesh Kshirsagar <yogeshkshirsagar2304@gmail.com>
2026-08-04 01:31:46 +0530

    Time: 237 ms (5.06%), Space: 23.9 MB (56.56%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,13 +3,16 @@ public:
     void moveZeroes(vector<int>& nums) {
 
         for(int i = 0 ; i < nums.size()-1; i++){
+            int swap = 0;
             for(int j = 0 ; j < nums.size()-1-i;j++){
                 if(nums[j] == 0){
                     int temp = nums[j];
                     nums[j] = nums[j+1];
                     nums[j+1] = temp;
+                    swap++;
                 }
             }
+            if(swap == 0) break;
         }
     }
 };
