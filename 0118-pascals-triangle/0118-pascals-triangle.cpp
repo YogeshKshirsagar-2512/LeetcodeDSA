de7bae78aae42690d6e3d05e0dd404ab8af925af
Yogesh Kshirsagar <yogeshkshirsagar2304@gmail.com>
2026-08-09 17:22:46 +0530

    Time: 0 ms (100%), Space: 9.6 MB (63.86%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -7,11 +7,9 @@ public:
             ans.push_back(vector<int>(i,1));
         }
 
-        for(int i = 0 ; i < ans.size(); i++){
-            for(int j = 0 ; j < ans[i].size(); j++){
-                if(j != 0 && j != i){
-                    ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
-                }
+        for(int i = 2 ; i < ans.size(); i++){
+            for(int j = 1 ; j < ans[i].size()-1; j++){
+                ans[i][j] = ans[i-1][j-1] + ans[i-1][j]; 
             }
         }
         return ans;
