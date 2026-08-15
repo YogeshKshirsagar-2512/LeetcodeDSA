ab2ffa585fe7cd6825d860e7574563f5eb29e298
Yogesh Kshirsagar <yogeshkshirsagar2304@gmail.com>
2026-08-15 18:29:21 +0530

    Time: 234 ms (5.38%), Space: 47.8 MB (57.76%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,28 +1,15 @@
-
-import java.util.Vector;
 class Solution {
     public void moveZeroes(int[] nums) {
 
-        Vector<Integer> ans = new Vector<>();
-        int zeroes = 0;
-        for(int i = 0 ;  i < nums.length ; i++){
-            if(nums[i] == 0){
-                zeroes++;
-            }else{
-                ans.add(nums[i]);
+        for(int i = 0 ; i < nums.length -1 ; i++){
+            for(int j = 0 ; j < nums.length -1-i; j++){
+                if(nums[j] == 0){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
             }
         }
-        for(int i = 0; i < zeroes; i++){
-            ans.add(0);
-        }
-        for(int i = 0; i < ans.size(); i++){
-            nums[i] = ans.get(i);
-        }
-
+        
     }
 }
-
-
-
-        
-    
