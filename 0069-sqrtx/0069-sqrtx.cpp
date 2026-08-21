2c4fd3443d6990a57888e21db6a6159c850434e6
Yogesh Kshirsagar <yogeshkshirsagar2304@gmail.com>
2026-08-22 00:08:38 +0530

    Time: 57 ms (6.54%), Space: 8.5 MB (87.5%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        for(int i = 1 ;  i <= x ; i++){
+            if(i == x/i) return i;
+            if(i > x/i) return i-1;
+        }
+        return 0;
+        
+    }
+};
