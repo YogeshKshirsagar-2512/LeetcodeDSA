d1c5faefa1129804648a36c0c8c939293b7fd8a9
Yogesh Kshirsagar <yogeshkshirsagar2304@gmail.com>
2026-08-25 09:42:48 +0530

    Time: 0 ms (100%), Space: 9.9 MB (53.14%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool NotValid(char ch) {
+        if(97 <= ch &&  ch <= 122) return false;
+        if(48 <= ch &&  ch <= 57) return false;
+        else return true;
+    }
+    bool isPalindrome(string s) {
+
+        int i = 0 ;
+        int j = s.size()-1;
+
+        while(i < j){
+            char x = s[i];
+            char y = s[j];
+
+            if(65 <= x &&  x <= 90) x += 32;
+            if(65 <= y &&  y <= 90) y += 32;
+
+            if(NotValid(x)) i++;
+            else if(NotValid(y)) j--;
+            else{
+                if(x != y) return false;
+                i++;
+                j--;
+            }
+
+
+        }
+        
+            return true;
+    }
+};
